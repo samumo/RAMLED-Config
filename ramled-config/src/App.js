f85bf1bd141041c77f91fbe861258a74056861c5
Samuel Morgan <74990386+samumo@users.noreply.github.com>
2021-06-24 07:46:29 +0100

    general development to App.js

--- ramled-config/src/App.js
+++ ramled-config/src/App.js
@@ -9,7 +9,6 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import Alert from 'react-bootstrap/Alert';
 import Table from 'react-bootstrap/Table';
-import Badge from 'react-bootstrap/Badge'
 
 // document.body.style = 'background: black;';
 
@@ -20,14 +19,21 @@ class Generator extends React.Component {
       // jsonData is stored as array of addresses(strings), their position in the array will be used as their eventual "num" value
       // json export format => [{"name": "<address string>", "value": 255, "num": 0}, ...] value property sets the default brightness, this can be 255 for all.
       jsonData: ["b-01-01", "b-01-02", "b-01-03"],
-      blockInput: {name: "name", numToAdd: 1, startNum: 0},
-      sequenceInput: {address: "", floorStart: 1, floorEnd: 7, apartmentSequence: [1,3,5,7]},
+      previousJsonData: ["b-01-01", "b-01-02", "b-01-03"],
+      blockInput: {name: "a-01-01", numToAdd: 1, startNum: 0},
+      sequenceInput: {startNum: 0, separator: '-', building: 'a', floorStart: 1, floorEnd: 7, apartmentSequence: [1,3,5,7]},
     }
-    // Bind here...
+    // Bindings
     this.handleChangeName = this.handleChangeName.bind(this);
     this.handleExecuteBlock = this.handleExecuteBlock.bind(this);
-    this.handleChangeStartNum = this.handleChangeStartNum.bind(this);
-    this.handleChangeNumToAdd = this.handleChangeNumToAdd.bind(this);
+    this.handleChangeBlockStartNum = this.handleChangeBlockStartNum.bind(this);
+    this.handleChangeBlockNumToAdd = this.handleChangeBlockNumToAdd.bind(this);
+    this.handleUndo = this.handleUndo.bind(this);
+    this.handleClearNames = this.handleClearNames.bind(this);
+    this.handleChangeSequenceStartNum = this.handleChangeSequenceStartNum.bind(this);
+    this.handleChangeSequenceBuilding = this.handleChangeSequenceBuilding.bind(this);
+    this.handleChangeSequenceFloorStart = this.handleChangeSequenceFloorStart.bind(this);
+    this.handleChangeSequenceFloorEnd = this.handleChangeSequenceFloorEnd.bind(this);
   }
 
   validateNumeric(input) {
@@ -46,15 +52,8 @@ class Generator extends React.Component {
     }
   }
 
-  validateNonZero(input) {
-    if(input === null) {
-      return null
-    } else {
-      return input
-    }
-  }
   // Input handler for Block Add 'startNum'
-  handleChangeStartNum(event) {
+  handleChangeBlockStartNum(event) {
     // Deep clone of state object made with stringify
     let blockStateObjectClone = JSON.parse(JSON.stringify(this.state.blockInput));
     // call to numeric validation function  
@@ -79,7 +78,7 @@ class Generator extends React.Component {
   }
 
   // Input handler for Block Add 'numToAdd'
-  handleChangeNumToAdd(event) {
+  handleChangeBlockNumToAdd(event) {
     let stateObjectClone = JSON.parse(JSON.stringify(this.state.blockInput));
     stateObjectClone['numToAdd'] = this.validateNumeric(event.target.value);
     this.setState({
@@ -87,6 +86,7 @@ class Generator extends React.Component {
     });
   }
 
+  //  method to generate block add array
   blockArray(name, ledCount) {
     let output = [];
     for(let i = 0; i < ledCount; i++) {
@@ -99,34 +99,122 @@ class Generator extends React.Component {
   handleExecuteBlock(event) {
     // This call prevents the submit button's default behaviour of reloading the page
     event.preventDefault()
-    // Deep copy of state 'jsonData'
+    // Deep working copy of state 'jsonData'
     let nameArray = [...this.state.jsonData];
+    // copy of state block input object 
     let stateObjectClone = JSON.parse(JSON.stringify(this.state.blockInput));
-    
     // numToAdd null value caught
     if(stateObjectClone['numToAdd'] === null) stateObjectClone['numToAdd'] = 0;
-
+    // call to array generator method
     let arrayToAdd = this.blockArray(stateObjectClone['name'], stateObjectClone['numToAdd']);
-    
-
-    //
     // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    // Needs some review to work as expected
-    // 
-    if(stateObjectClone['startNum'] === null || stateObjectClone['startNum'] > nameArray.length) {
-      // new names addes at end of
+    // Consider revision for alternative behaviour
+    // catching out of range error and adding new array to the end
+    if(stateObjectClone['startNum'] > nameArray.length) {
       nameArray = [...nameArray, ...arrayToAdd];
     } else {
+      // default behaviour of adding new array at specified index
       nameArray.splice(this.state.blockInput['startNum'], 0, ...arrayToAdd);
     }
+    // copy of state 'jsonData' for undo
+    let jsonDataPrevious = [...this.state.jsonData];
 
     this.setState({
+      previousJsonData: jsonDataPrevious,
       jsonData: nameArray,
-      blockInput: {name: "name", numToAdd: 1, startNum: nameArray.length},
+      blockInput: {name: stateObjectClone['name'], numToAdd: stateObjectClone['numToAdd'], startNum: nameArray.length},
+    });
+  }
+  // method to handle undo to json
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  // Consider revision for multiple undo, currently this only supports one undo
+  handleUndo(event) {
+    // copy of state 'jsonData' for undo
+    let jsonDataPrevious = [...this.state.previousJsonData];
+    this.setState({
+      jsonData: jsonDataPrevious
     });
   }
 
+  // method to clear 'jsonData'
+  handleClearNames(event) {
+    // copy of state 'jsonData' for undo
+    let jsonDataPrevious = [...this.state.jsonData];
+    this.setState({
+      jsonData: [],
+      previousJsonData: jsonDataPrevious,
+    })
+  }
+
+  // handler for input of sequence start number
+  handleChangeSequenceStartNum(event) {
+    // Deep clone of state object made with stringify
+    let sequenceStateObjectClone = JSON.parse(JSON.stringify(this.state.sequenceInput));
+    // call to numeric validation function  
+    sequenceStateObjectClone['startNum'] = this.validateNumeric(event.target.value);
+    this.setState({
+      sequenceInput: sequenceStateObjectClone,
+    });
+  };
+
+  // validate building name to 2 characters
+  validateBuilding(input) {
+    if(input === null) {
+      return input;
+    } else {
+      return input.match(/[\w\d]{0,2}/);
+    }
+  };
+
+  // handler for sequence building name
+  handleChangeSequenceBuilding(event) {
+    // Deep clone of state object made with stringify
+    let sequenceStateObjectClone = JSON.parse(JSON.stringify(this.state.sequenceInput));
+    // call to numeric validation function  
+    sequenceStateObjectClone['building'] = this.validateBuilding(event.target.value);
+    this.setState({
+      sequenceInput: sequenceStateObjectClone,
+    });
+  }
+
+    // validate building name to 2 numeric characters
+    validateFloorStartEnd(input) {
+      if(input === null) {
+        return input;
+      } else {
+        return input.match(/[\d]{0,2}/);
+      }
+    };
+
+  // handler for sequence floor start
+  handleChangeSequenceFloorStart(event) {
+    // Deep clone of state object made with stringify
+    let sequenceStateObjectClone = JSON.parse(JSON.stringify(this.state.sequenceInput));
+    // call to numeric validation function  
+    sequenceStateObjectClone['floorStart'] = this.validateFloorStartEnd(event.target.value);
+    this.setState({
+      sequenceInput: sequenceStateObjectClone,
+    });
+  }
+
+  // handler for sequence floor end
+  handleChangeSequenceFloorEnd(event) {
+    // Deep clone of state object made with stringify
+    let sequenceStateObjectClone = JSON.parse(JSON.stringify(this.state.sequenceInput));
+    // call to numeric validation function  
+    sequenceStateObjectClone['floorEnd'] = this.validateFloorStartEnd(event.target.value);
+    this.setState({
+      sequenceInput: sequenceStateObjectClone,
+    });
+  }
+
+
+
   render() {
+    const btnBlockAdd = <Button onClick={this.handleExecuteBlock} variant="success">Add Names</Button>;
+    const btnUndo = <Button onClick={this.handleUndo} variant="warning">Undo</Button>;
+    const btnClearJson = <Button onClick={this.handleClearNames} variant="danger">Clear Names</Button>;
+    const btnSeqAdd = <Button onClick={this.handleExecuteSequence} variant="success">Add Names</Button>;
     return (
       <div>
         <br />
@@ -139,7 +227,7 @@ class Generator extends React.Component {
               </InputGroup.Prepend>
               <FormControl placeholder=''
                            value={this.state.blockInput['startNum']}
-                           onChange={this.handleChangeStartNum.bind(this)}/>
+                           onChange={this.handleChangeBlockStartNum.bind(this)}/>
             </InputGroup>
 
             <InputGroup className="mb-3">
@@ -157,13 +245,13 @@ class Generator extends React.Component {
               </InputGroup.Prepend>
               <FormControl placeholder=''
                            value={this.state.blockInput['numToAdd']}
-                           onChange={this.handleChangeNumToAdd.bind(this)}/>
+                           onChange={this.handleChangeBlockNumToAdd.bind(this)}/>
             </InputGroup>
             
             <ButtonGroup>
-              <Button onClick={this.handleExecuteBlock} variant="primary">Add Names</Button>{' '}
-              <Button variant="secondary">Clear Form</Button>
-              <Button variant="warning">undo?</Button>
+              {btnBlockAdd}
+              {btnUndo}
+              {btnClearJson}
             </ButtonGroup>
           </Tab>
           <Tab eventKey="add-sequence-of-names" title="Add Sequence of Names">
@@ -172,31 +260,29 @@ class Generator extends React.Component {
               <InputGroup.Prepend>
                 <InputGroup.Text>START AT LED NUMBER</InputGroup.Text>
               </InputGroup.Prepend>
-              <FormControl />
-            </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Prepend>
-                <InputGroup.Text>SEPARATOR</InputGroup.Text>
-              </InputGroup.Prepend>
-              <FormControl />          
+              <FormControl value={this.state.sequenceInput['startNum']} 
+                           onChange={this.handleChangeSequenceStartNum.bind(this)}/>
             </InputGroup>
 
             <InputGroup className="mb-3">
               <InputGroup.Prepend>
                 <InputGroup.Text>BUILDING</InputGroup.Text>
               </InputGroup.Prepend>
-              <FormControl />
+              <FormControl value={this.state.sequenceInput['building']}
+                           onChange={this.handleChangeSequenceBuilding.bind(this)}/>
             </InputGroup >
 
             <InputGroup  className="mb-3">
               <InputGroup.Prepend>
                 <InputGroup.Text>FLOOR START</InputGroup.Text>
               </InputGroup.Prepend>
-              <FormControl />            
+              <FormControl value={this.state.sequenceInput['floorStart']}
+                           onChange={this.handleChangeSequenceFloorStart.bind(this)}/>            
               <InputGroup.Prepend>
                 <InputGroup.Text>FLOOR END</InputGroup.Text>
               </InputGroup.Prepend>
-              <FormControl />
+              <FormControl value={this.state.sequenceInput['floorEnd']}
+                           onChange={this.handleChangeSequenceFloorEnd.bind(this)}/>    
             </InputGroup>
 
             <InputGroup className="mb-3">
@@ -206,8 +292,9 @@ class Generator extends React.Component {
               <FormControl />
             </InputGroup>
             <ButtonGroup>
-              <Button variant="primary">Add names</Button>{' '}
-              <Button variant="secondary">Clear Form</Button>
+              {btnSeqAdd}
+              {btnUndo}
+              {btnClearJson}
             </ButtonGroup>
           </Tab>
           <Tab eventKey="edit-names" title="Edit/Delete Names">
